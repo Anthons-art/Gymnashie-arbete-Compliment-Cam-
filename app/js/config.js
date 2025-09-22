b34793544f2602d14713de96d84362444995526b
Anthons-art <anthon.axelsson@elev.ga.ntig.se>
2025-09-22 11:34:25 +0200

    version 1

---
+++ app/js/config.js
@@ -0,0 +1,14 @@
+export const cfg = {
+  detection: {
+    minProbability: 0.65,
+    minFaceFrac: 0.10,
+    useROI: true,
+    stableFrames: 50
+  },
+  timings: {
+    wave: 400,
+    speak: 1600,
+    cooldown: 3500
+  },
+  detectionIntervalMs: 80
+};
